225b405a6a1be866af78605faa6ee7b347ebe3cc
Shaik Suhana <suhanashaik044@gmail.com>
2026-05-01 23:52:33 +0530

    j

--- QuestionAPP/demo/src/main/java/Questiondb/demo/DemoApplication.java
+++ QuestionAPP/demo/src/main/java/Questiondb/demo/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
-@SpringBootApplication
 
+@SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
