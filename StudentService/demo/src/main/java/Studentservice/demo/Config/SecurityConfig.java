303198976a2d2503aeb0e4b52b1e044efc0b3b60
Shaik Suhana <suhanashaik044@gmail.com>
2026-05-04 18:42:07 +0530

    f

--- StudentService/demo/src/main/java/Studentservice/demo/Config/SecurityConfig.java
+++ StudentService/demo/src/main/java/Studentservice/demo/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ package Studentservice.demo.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,7 +37,7 @@ public class SecurityConfig {
                         // 🔥 VERY IMPORTANT
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> {});
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
