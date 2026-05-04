9b3a00acccee15de21691c1521497115f9a417b2
Shaik Suhana <suhanashaik044@gmail.com>
2026-05-04 23:28:04 +0530

    d

--- StudentService/demo/src/main/java/Studentservice/demo/Service/StudentService.java
+++ StudentService/demo/src/main/java/Studentservice/demo/Service/StudentService.java
@@ -31,13 +31,15 @@ public class StudentService {
 
 
     public List<Question> getQuestionsFromQuestionService(){
-
+        System.out.println("NEW CODE EXECUTED");
         return webClientBuilder.build()
                 .get()
                 .uri("https://examsystem-4.onrender.com/questions/random") // ✅ deployed URL
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<List<Question>>() {}) // ✅ FIX
                 .block();
+
+
     }
 
 
