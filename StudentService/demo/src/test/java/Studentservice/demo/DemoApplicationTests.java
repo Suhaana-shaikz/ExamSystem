b4ad7beeb08aab5e20577008481eaf28a40d2ab6
Shaik Suhana <suhanashaik044@gmail.com>
2026-09-06 00:51:38 +0530

    f

--- StudentService/demo/src/test/java/Studentservice/demo/DemoApplicationTests.java
+++ StudentService/demo/src/test/java/Studentservice/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package Studentservice.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Studentservice.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
